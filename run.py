458c3a054d617e7be94e0a033a9c2accfcb0510e
Your Name <your.email@example.com>
2025-04-24 16:08:35 +0200

    Configurando para despliegue en Render

--- run.py
+++ run.py
@@ -3,11 +3,10 @@ import os
 
 app = create_app()
 
-# إنشاء قاعدة البيانات عند تشغيل التطبيق
-@app.before_first_request
-def initialize_database():
+# Inicializar la base de datos con el contexto de la aplicación
+with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
